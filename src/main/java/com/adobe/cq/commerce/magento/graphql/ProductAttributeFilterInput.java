2bc5519d17fa3bc0f84065bdc9eb7e3929b3bc9f
Mark J. Becker <herzog31@users.noreply.github.com>
2022-07-07 17:01:37 +0200

    CIF-2895 - Extend ProductAttributeFilterInput with custom filters

--- src/main/java/com/adobe/cq/commerce/magento/graphql/ProductAttributeFilterInput.java
+++ src/main/java/com/adobe/cq/commerce/magento/graphql/ProductAttributeFilterInput.java
@@ -15,6 +15,10 @@
 package com.adobe.cq.commerce.magento.graphql;
 
 import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.shopify.graphql.support.Input;
 
@@ -54,6 +58,8 @@ public class ProductAttributeFilterInput implements Serializable {
     private Input<FilterEqualTypeInput> sku = Input.undefined();
 
     private Input<FilterEqualTypeInput> urlKey = Input.undefined();
+
+    private Map<String, Input<Serializable>> customFilters = new HashMap<>();
 
     /**
      * Deprecated: use `category_uid` to filter product by category ID.
@@ -583,9 +589,35 @@ public class ProductAttributeFilterInput implements Serializable {
         return this;
     }
 
+    public ProductAttributeFilterInput setCustomFilter(String name, Serializable filterInput) {
+        this.customFilters.put(name, Input.optional(filterInput));
+        return this;
+    }
+
     public void appendTo(StringBuilder _queryBuilder) {
         String separator = "";
         _queryBuilder.append('{');
+
+        if (!this.customFilters.isEmpty()) {
+            for (Map.Entry<String, Input<Serializable>> entry : customFilters.entrySet()) {
+                _queryBuilder.append(separator);
+                separator = ",";
+                _queryBuilder.append(entry.getKey() + ":");
+
+                Serializable filter = entry.getValue().getValue();
+
+                if (filter != null) {
+                    try {
+                        Method appendTo = filter.getClass().getMethod("appendTo", StringBuilder.class);
+                        appendTo.invoke(filter, _queryBuilder);
+                    } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+                        _queryBuilder.append("null");
+                    }
+                } else {
+                    _queryBuilder.append("null");
+                }
+            }
+        }
 
         if (this.categoryId.isDefined()) {
             _queryBuilder.append(separator);
